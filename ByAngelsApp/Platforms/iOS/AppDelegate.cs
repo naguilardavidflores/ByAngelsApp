f0ff48cd622c1da6790835c87aed37ecc793bb04
Nelson9024 <naguilardavid@gmail.com>
2026-06-13 15:02:23 -0500

    Ddesde 0

--- ByAngelsApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ByAngelsApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
